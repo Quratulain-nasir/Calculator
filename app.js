4661e23f5e5484ab197aab83a2d0a1a975ebc330
Quratulain-nasir <125543412+Quratulain-nasir@users.noreply.github.com>
2023-05-18 13:12:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -15,53 +15,53 @@ function getResults(){
     document.getElementById('result').value = eval(document.getElementById('result').value)
 }
 
-function mathlog() {
-    document.getElementById('result').value= eval(Math.log(document.getElementById('result').value));
-}
+// function mathlog() {
+//     document.getElementById('result').value= eval(Math.log(document.getElementById('result').value));
+// }
 
-function mathsin() {
-    document.getElementById('result').value= eval(Math.sin(document.getElementById('result').value));
-}
+// function mathsin() {
+//     document.getElementById('result').value= eval(Math.sin(document.getElementById('result').value));
+// }
 
-function mathcos() {
-    document.getElementById('result').value= eval(Math.cos(document.getElementById('result').value));
-}
+// function mathcos() {
+//     document.getElementById('result').value= eval(Math.cos(document.getElementById('result').value));
+// }
 
-function mathtan() {
-    document.getElementById('result').value= eval(Math.tan(document.getElementById('result').value));
-}
+// function mathtan() {
+//     document.getElementById('result').value= eval(Math.tan(document.getElementById('result').value));
+// }
 
-function Exp() {
-    document.getElementById('result').value= eval(Math.E*document.getElementById('result').value);
-}
+// function Exp() {
+//     document.getElementById('result').value= eval(Math.E*document.getElementById('result').value);
+// }
 
-function square(){
-    var square =document.getElementById('result').value*document.getElementById('result').value
-    document.getElementById('result').value= square;
-}
+// function square(){
+//     var square =document.getElementById('result').value*document.getElementById('result').value
+//     document.getElementById('result').value= square;
+// }
 
-function mathlog10() {
-    document.getElementById('result').value= eval(Math.log10(document.getElementById('result').value));
-}
+// function mathlog10() {
+//     document.getElementById('result').value= eval(Math.log10(document.getElementById('result').value));
+// }
 
-function mathpi() {
-    document.getElementById('result').value= eval(Math.PI*document.getElementById('result').value);
-}
+// function mathpi() {
+//     document.getElementById('result').value= eval(Math.PI*document.getElementById('result').value);
+// }
 
-function mathrad() {
-    document.getElementById('result').value= eval(document.getElementById('result').value*(Math.PI/100));
-}
+// function mathrad() {
+//     document.getElementById('result').value= eval(document.getElementById('result').value*(Math.PI/100));
+// }
 
-function mathdeg() {
-    document.getElementById('result').value= eval(document.getElementById('result').value*(180/Math.PI));
-}
+// function mathdeg() {
+//     document.getElementById('result').value= eval(document.getElementById('result').value*(180/Math.PI));
+// }
 
-function mathper() {
-    document.getElementById('result').value= eval(document.getElementById('result').value/100);
-}
+// function mathper() {
+//     document.getElementById('result').value= eval(document.getElementById('result').value/100);
+// }
 
-function mathsqrt() {
-    document.getElementById('result').value= eval(Math.sqrt(document.getElementById('result').value));
-}
+// function mathsqrt() {
+//     document.getElementById('result').value= eval(Math.sqrt(document.getElementById('result').value));
+// }
 
 
